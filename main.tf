528d1cfd954fc1f6ce5a0e3bfd30786979f4fe1c
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-11-22 19:59:04 +0100

    update

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-data "external" "local_install" {
-  program = ["bash", "${path.module}/scripts/data.sh"]
-}
-
-resource "null_resource" "local_install" {
-  depends_on = ["data.external.local_install"]
-
-  provisioner "local-exec" {
-    command = "bash ${path.module}/scripts/null.sh"
-  }
-
-  triggers {
-    timestamp = "${timestamp()}"
-  }
-}
-
-resource "null_resource" "local_install_on_destroy" {
-  depends_on = ["data.external.local_install"]
-
-  provisioner "local-exec" {
-    command = "bash ${path.module}/scripts/null.sh"
-    when    = "destroy"
-  }
-}
-
 provider "kubernetes" {}
 
 resource "kubernetes_replication_controller" "example" {
